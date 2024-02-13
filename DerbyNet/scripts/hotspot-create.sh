2f558fb74f0be1a867fd66878c3006df8f0a5e03
Tony Ellard <4236212+tonyellard@users.noreply.github.com>
2024-02-13 15:39:28 -0600

    update ssid

--- DerbyNet/scripts/hotspot-create.sh
+++ DerbyNet/scripts/hotspot-create.sh
@@ -1,4 +1,4 @@
-nmcli con add type wifi ifname wlan0 con-name Hotspot autoconnect yes ssid Hotspot
+nmcli con add type wifi ifname wlan0 con-name Hotspot autoconnect yes ssid Derbynet825
 nmcli con modify Hotspot 802-11-wireless.mode ap 802-11-wireless.band bg ipv4.method shared
 nmcli con modify Hotspot wifi-sec.key-mgmt wpa-psk
 nmcli con modify Hotspot wifi-sec.psk "Let'sgo825"
